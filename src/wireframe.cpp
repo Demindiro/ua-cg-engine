7c85afa366a5b74471d1ab648386fae0175c5fbe
David Hoppenbrouwers <david@salt-inc.org>
2022-03-25 23:50:37 +0100

    Attempt at implementing dodecahedron triangles
    It's wrong but it's too much effort to fix it now.

--- src/wireframe.cpp
+++ src/wireframe.cpp
@@ -343,21 +343,23 @@ namespace wireframe {
 		platonic(conf, mat_project, triangles, points, 12, faces, 20);
 	}
 
-	static void dodecahedron(ini::Section &conf, Matrix &mat_project, vector<Line3D> &lines) {
-		Vector3D points[20];
-		Edge edges[30];
+	static void dodecahedron(Vector3D points[20]) {
 		Vector3D ico[12];
 		icosahedron(ico);
 		for (int i = 0; i < 5; i++) {
 			int j = (i + 1) % 5;
-
 			// Top & bottom "hat"
 			points[0 + i] = (ico[0] + ico[2 + i] + ico[2 + j]) / 3;
 			points[5 + i] = (ico[1] + ico[7 + i] + ico[7 + j]) / 3;
 			// Ring
 			points[10 + i] = (ico[2 + i] + ico[7 + i] + ico[7 + j]) / 3;
 			points[15 + i] = (ico[2 + i] + ico[2 + j] + ico[7 + j]) / 3;
+		}
+	}
 
+	static void dodecahedron(Edge edges[30]) {
+		for (int i = 0; i < 5; i++) {
+			int j = (i + 1) % 5;
 			// Top & bottom ring
 			edges[0 + i] = { 0 + i, 0 + j };
 			edges[5 + i] = { 5 + i, 5 + j };
@@ -368,7 +370,50 @@ namespace wireframe {
 			edges[20 + i] = { 10 + i, 15 + i };
 			edges[25 + i] = { 10 + j, 15 + i };
 		}
+	}
+
+	static void dodecahedron(Face faces[36]) {
+		// FIXME Copied straight from cursus but we have different points so
+		// it resembles an eldritch horror.
+		// Deducing the points manually is currently very hard since we can't
+		// see the edges between faces. When lighting based on normals is
+		// implemented we can fix it more easily.
+		struct { int a, b, c, d, e; } fives[12] = {
+			{  1,  2,  3,  4,  5 },
+			{  1,  6,  7,  8,  2 },
+			{  2,  8,  9, 10,  3 },
+			{  3, 10, 11, 12,  4 },
+			{  4, 12, 13, 14,  5 },
+			{  5, 14, 15,  6,  1 },
+			{ 20, 19, 18, 17, 16 },
+			{ 20, 15, 14, 13, 19 },
+			{ 19, 13, 12, 11, 18 },
+			{ 18, 11, 10,  9, 17 },
+			{ 17,  9,  8,  7, 16 },
+			{ 16,  7,  6, 15, 20 },
+		};
+
+		for (int i = 0; i < 12; i++) {
+			faces[i * 3 + 0] = { fives[i].a - 1, fives[i].b - 1, fives[i].c - 1 };
+			faces[i * 3 + 1] = { fives[i].a - 1, fives[i].c - 1, fives[i].d - 1 };
+			faces[i * 3 + 2] = { fives[i].a - 1, fives[i].d - 1, fives[i].e - 1 };
+		}
+	}
+
+	static void dodecahedron(ini::Section &conf, Matrix &mat_project, vector<Line3D> &lines) {
+		Vector3D points[20];
+		Edge edges[30];
+		dodecahedron(points);
+		dodecahedron(edges);
 		platonic(conf, mat_project, lines, points, 20, edges, 30);
+	}
+
+	static void dodecahedron(ini::Section &conf, Matrix &mat_project, vector<Triangle3D> &triangles) {
+		Vector3D points[20];
+		Face faces[36];
+		dodecahedron(points);
+		dodecahedron(faces);
+		platonic(conf, mat_project, triangles, points, 20, faces, 36);
 	}
 
 	static void circle(vector<Vector3D> &points, int n, double z) {
@@ -688,9 +733,9 @@ namespace wireframe {
 				octahedron(fig, mat_eye, triangles);
 			} else if (type == "Icosahedron") {
 				icosahedron(fig, mat_eye, triangles);
-				/*
 			} else if (type == "Dodecahedron") {
-				dodecahedron(fig, mat_eye, lines);
+				dodecahedron(fig, mat_eye, triangles);
+				/*
 			} else if (type == "Cylinder") {
 				cylinder(fig, mat_eye, lines);
 			} else if (type == "Cone") {
