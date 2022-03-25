2363eb1c6a031fce4f85794b978da964f11ce5e9
David Hoppenbrouwers <david@salt-inc.org>
2022-03-25 22:46:38 +0100

    Fix projection of 3D lines

--- src/lines.cpp
+++ src/lines.cpp
@@ -40,25 +40,6 @@ static img::EasyImage create_img(
 	return img;
 }
 
-template<class T>
-static img::EasyImage create_img(T &lines, uint size, Color background, double &d, double &offset_x, double &offset_y) {
-	if (lines.empty()) {
-		return img::EasyImage(0, 0);
-	}
-
-	// Determine bounds
-	auto p = lines[0].a;
-	double min_x = p.x, min_y = p.y, max_x = p.x, max_y = p.y;
-	for (auto &l : lines) {
-		min_x = min(min(min_x, l.a.x), l.b.x);
-		max_x = max(max(max_x, l.a.x), l.b.x);
-		min_y = min(min(min_y, l.a.y), l.b.y);
-		max_y = max(max(max_y, l.a.y), l.b.y);
-	}
-
-	return create_img(min_x, min_y, max_x, max_y, size, background, d, offset_x, offset_y);
-}
-
 inline void Line2D::draw(img::EasyImage &img) const {
 	img.draw_line(round_up(a.x), round_up(a.y), round_up(b.x), round_up(b.y), color);
 }
@@ -68,8 +49,23 @@ void Lines2D::add(Line2D line) {
 }
 
 img::EasyImage Lines2D::draw(uint size, Color background) const {
+	if (lines.empty()) {
+		return img::EasyImage(0, 0);
+	}
+
+	// Determine bounds
+	double min_x, max_x, min_y, max_y;
+	min_x = min_y = +numeric_limits<double>::infinity();
+	max_x = max_y = -numeric_limits<double>::infinity();
+	for (auto &l : lines) {
+		min_x = min(min(min_x, l.a.x), l.b.x);
+		max_x = max(max(max_x, l.a.x), l.b.x);
+		min_y = min(min(min_y, l.a.y), l.b.y);
+		max_y = max(max(max_y, l.a.y), l.b.y);
+	}
+
 	double d, offset_x, offset_y;
-	auto img = create_img(this->lines, size, background, d, offset_x, offset_y);
+	auto img = create_img(min_x, min_y, max_x, max_y, size, background, d, offset_x, offset_y);
 
 	// Transform & draw lines
 	for (auto &l : this->lines) {
@@ -91,21 +87,36 @@ void Lines3D::add(Line3D line) {
 
 img::EasyImage Lines3D::draw(uint size, Color background, bool with_z) const {
 	// TODO wdym "unitialized", GCC?
-	double d = NAN, offset_x = NAN, offset_y = NAN;
-	auto img = create_img(this->lines, size, background, d, offset_x, offset_y);
+	if (lines.empty()) {
+		return img::EasyImage(0, 0);
+	}
+
+	// Determine bounds
+	double min_x, max_x, min_y, max_y;
+	min_x = min_y = +numeric_limits<double>::infinity();
+	max_x = max_y = -numeric_limits<double>::infinity();
+	for (auto &l : lines) {
+		min_x = min(min(min_x, l.a.x / -l.a.z), l.b.x / -l.b.z);
+		max_x = max(max(max_x, l.a.x / -l.a.z), l.b.x / -l.b.z);
+		min_y = min(min(min_y, l.a.y / -l.a.z), l.b.y / -l.b.z);
+		max_y = max(max(max_y, l.a.y / -l.a.z), l.b.y / -l.b.z);
+	}
+
+	double d, offset_x, offset_y;
+	auto img = create_img(min_x, min_y, max_x, max_y, size, background, d, offset_x, offset_y);
 
 	// Transform & draw lines
 	if (with_z) {
 		ZBuffer z(img.get_width(), img.get_height());
 		for (auto &l : this->lines) {
-			Point3D a(l.a.x * d + offset_x, l.a.y * d + offset_y, l.a.z);
-			Point3D b(l.b.x * d + offset_x, l.b.y * d + offset_y, l.b.z);
+			Point3D a(l.a.x / -l.a.z * d + offset_x, l.a.y / -l.a.z * d + offset_y, l.a.z);
+			Point3D b(l.b.x / -l.b.z * d + offset_x, l.b.y / -l.b.z * d + offset_y, l.b.z);
 			Line3D(a, b, l.color).draw(img, z);
 		}
 	} else {
 		for (auto &l : this->lines) {
-			Point2D a(l.a.x * d + offset_x, l.a.y * d + offset_y);
-			Point2D b(l.b.x * d + offset_x, l.b.y * d + offset_y);
+			Point2D a(l.a.x / -l.a.z * d + offset_x, l.a.y / -l.a.z * d + offset_y);
+			Point2D b(l.b.x / -l.b.z * d + offset_x, l.b.y / -l.b.z * d + offset_y);
 			Line2D(a, b, l.color).draw(img);
 		}
 	}
@@ -126,7 +137,6 @@ img::EasyImage Triangles3D::draw(uint size, Color background) const {
 	}
 
 	// Determine bounds
-	auto p = triangles[0].a;
 	double min_x, max_x, min_y, max_y;
 	min_x = min_y = +numeric_limits<double>::infinity();
 	max_x = max_y = -numeric_limits<double>::infinity();
