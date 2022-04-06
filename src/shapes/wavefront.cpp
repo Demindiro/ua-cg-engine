e38d2cd46b71b6dc492ba68a483d172fb3649293
David Hoppenbrouwers <david@salt-inc.org>
2022-04-06 04:55:53 +0200

    Implement custom Wavefront parser.
    The previous one used waay too much memory and even after optimizations
    it was quite slow. The new parser is:
    
    - **Much** shorter & simpler
    - A little slower (~98s -> ~101s for Lucy)
    - Uses much less memory (5.4G -> 2.6G!)
    
    It is also much easier to improve further with e.g. a custom allocator,
    though it is very likely the biggest bottleneck now is std::stod, and
    that's hard to replace without external libraries :(

--- src/shapes/wavefront.cpp
+++ src/shapes/wavefront.cpp
@@ -1,8 +1,11 @@
 #include "shapes/wavefront.h"
 #include <fstream>
+#include <iterator>
+#include <limits>
 #include <set>
+#include <string>
+#include <sstream>
 #include "ini_configuration.h"
-#include "obj_parser.h"
 #include "math/point3d.h"
 #include "shapes.h"
 #include "render/triangle.h"
@@ -13,15 +16,26 @@ namespace shapes {
 using namespace std;
 using namespace render;
 
-void wavefront(const Configuration &conf, FaceShape &shape, Material &mat) {
-	auto obj = [&]() {
-		ifstream f(conf.section["file"].as_string_or_die());
-		cout << "Reading Wavefront file" << endl;
-		return obj::ObjectGroup(f);
-	}();
+// References:
+//
+// https://raw.githubusercontent.com/Alhadis/language-wavefront/v1.0.2/docs/obj-spec.pdf
+// https://www.fileformat.info/format/wavefrontobj/egff.htm
+// https://www.fileformat.info/format/material/
+//
+// Supported ntry types. All unrecognized entries will cause an exception.
+// - [ ] f (faces)
+// - [ ] g (group name)
+// - [ ] s (smoothing group)
+// - [ ] mtllib
+// - [ ] usemtl
+// - [ ] v (point)
+// - [ ] vt (uv)
+// - [ ] vn (normal)
+//
+// We're parsing it directly to avoid an excessively huge amount of allocations & avoid
+// unneccessary indirection in general.
 
-	cout << obj << endl;
-	cout << "Parsing loaded data" << endl;
+void wavefront(const Configuration &conf, FaceShape &shape, Material &mat) {
 
 	// Find all unique point/uv/normal triples & create points & faces
 	struct Triple {
@@ -30,66 +44,212 @@ void wavefront(const Configuration &conf, FaceShape &shape, Material &mat) {
 			return pi != r.pi ? pi < r.pi : (ti != r.ti ? ti < r.ti : ni < r.ni);
 		}
 	};
+
 	map<Triple, unsigned int> triples;
-	for (const auto &p : obj.get_polygons()) {
-		auto get = [&](auto i) {
-			auto pi = p.get_indexes().at(i);
-			auto ti = p.has_texture_indexes() ? p.get_texture_indexes().at(i) : 0;
-			auto ni = p.has_normal_indexes() ? p.get_normal_indexes().at(i) : 0;
-			// FIXME indices may be negative - yay
-			return Triple { pi - 1, ti - 1, ni - 1 };
-		};
-		for (size_t i = 0; i < p.get_indexes().size(); i++) {
-			auto t = get(i);
-			if (triples.count(t) == 0) {
-				triples[t] = (unsigned int)triples.size();
-				shape.points.push_back(tup_to_point3d(obj.get_vertexes().at(t.pi)));
-				shape.uvs.push_back(t.ti != -1
-					? tup_to_point2d(obj.get_texture_coordinates().at(t.ti))
-					: Point2D()
-				);
-				shape.normals.push_back(t.ni != -1
-					? tup_to_vector3d(obj.get_vertex_normals().at(t.ni))
-					: Vector3D()
-				);
+	string usemtl, mtllib;
+	vector<Point3D> points;
+	vector<Point2D> uvs;
+	vector<Vector3D> normals;
+
+	ifstream f(conf.section["file"].as_string_or_die());
+	cout << "Reading Wavefront file" << endl;
+	string line, prefix, token;
+	int no_uv = -1, no_normals = -1;
+
+	unsigned int line_i = 0;
+	while (getline(f, line)) {
+		line_i++;
+		istringstream tok(line);
+		istream_iterator<string> it(tok);
+		auto end = istream_iterator<string>();
+		if (it == end) {
+			// Empty line.
+			continue;
+		}
+
+		auto next_string = [&]() {
+			++it;
+			if (it == end) {
+				throw WavefrontParseException(line_i, "expected token");
 			}
-			assert(triples.size() == shape.points.size());
+			return *it;
+		};
+		auto next_double = [&]() {
+			return stod(next_string());
+		};
+		auto maybe_next_double = [&]() {
+			++it;
+			if (it == end) {
+				return numeric_limits<double>::signaling_NaN();
+			}
+			return stod(*it);
+		};
+		auto no_next = [&]() {
+			++it;
+			if (it != end) {
+				throw WavefrontParseException(line_i, "too many tokens");
+			}
+		};
+
+		if (*it == "v") {
+			auto x = next_double();
+			auto y = next_double();
+			auto z = next_double();
+			maybe_next_double();
+			points.push_back({ x, y, z });
+		} else if (*it == "vt") {
+			auto u = next_double();
+			auto v = maybe_next_double();
+			maybe_next_double();
+			uvs.push_back({ u, v });
+		} else if (*it == "vn") {
+			auto x = next_double();
+			auto y = next_double();
+			auto z = next_double();
+			normals.push_back({ x, y, z });
+		} else if (*it == "f") {
+			auto get = [&](auto str) {
+				istringstream s(str);
+				Triple t = { 0, 0, 0 };
+				if (!getline(s, token, '/')) {
+					throw WavefrontParseException(line_i, "face vertex must have at least one point");
+				}
+				t.pi = stoi(token);
+				if (getline(s, token, '/') && token != "") {
+					t.ti = stoi(token);
+				}
+				if (getline(s, token, '/')) {
+					t.ni = stoi(token);
+				}
+				if (triples.count(t) == 0) {
+					triples[t] = (unsigned int)triples.size();
+				}
+				return triples[t];
+			};
+			// Get first three required triples.
+			auto a = get(next_string());
+			auto b = get(next_string());
+			auto c = get(next_string());
+			shape.faces.push_back({ a, b, c });
+			// Push remaining triangles to form polygon.
+			while (++it != end) {
+				b = c;
+				c = get(*it);
+				shape.faces.push_back({ a, b, c });
+			}
+		} else if (*it == "s" || *it == "g") {
+			// Ignore anything group-related for now.
+			continue;
+		} else if (*it == "usemtl") {
+			auto s = next_string();
+			usemtl.swap(s);
+			if (!s.empty() && s != usemtl) {
+				throw WavefrontParseException(line_i, "multiple different materials are not supported");
+			}
+		} else if (*it == "mtllib") {
+			if (!mtllib.empty()) {
+				throw WavefrontParseException(line_i, "only one mtllib can be loaded");
+			}
+			mtllib = next_string();
+		} else if (it->size() > 0 && it->front() == '#') {
+			continue; // Skip no_next() at the end.
+		} else {
+			throw WavefrontParseException(line_i, "unknown keyword '" + *it + "'");
 		}
-		for (size_t i = 2; i < p.get_indexes().size(); i++) {
-			shape.faces.push_back({ triples[get(0)], triples[get(i - 1)], triples[get(i)] });
-		}
+		no_next();
 	}
 
-	if (obj.get_mtllib_file_name() != "") {
-		ifstream f(obj.get_mtllib_file_name());
-		obj::MTLLibrary mtl(f);
-		cout << mtl << endl;
+	// Construct actual vertexes from face triples.
+	shape.points.resize(triples.size());
+	shape.uvs.resize(triples.size());
+	shape.normals.resize(triples.size());
+	for (auto [t, i] : triples) {
+		assert(i < triples.size());
+		shape.points[i] = points.at(t.pi - 1);
+		shape.uvs[i] = t.ti > 0 ? uvs.at(t.ti - 1) : Point2D();
+		shape.normals[i] = t.ni > 0 ? normals.at(t.ni - 1) : Vector3D();
+	}
 
-		if (obj.get_mtl_name() != "") {
-			const auto &m = mtl[obj.get_mtl_name()];
-			vector<double> tup;
-			string str;
-			// TODO do we need to support per-light type textures? It seems
-			// excessive...
-			if (m["map_Ka"].as_string_if_exists(str)
-				|| m["map_Kd"].as_string_if_exists(str)
-				|| m["map_Ks"].as_string_if_exists(str)) {
-				img::EasyImage img;
-				ifstream f(str);
-				f >> img;
-				mat.texture.emplace(Texture(std::move(img)));
+	// Parse material if any is provided
+	if (mtllib != "" && usemtl != "") {
+		ifstream f(mtllib);
+		
+		// Find proper material
+		unsigned int line_i = 0;
+		while (getline(f, line)) {
+			line_i++;
+			istringstream s(line);
+			istream_iterator<string> it(s);
+			auto end = istream_iterator<string>();
+			if (it == end || *it != "newmtl") {
+				continue;
 			}
-			mat.reflection = m["Ns"].as_double_or_default(0);
+			goto found;
+		}
+		throw WavefrontParseException(0, "material " + usemtl + " not found");
+	found:
 
-			obj::DoubleTuple c;
-			auto try_from_conf = [&](auto e) {
-				return m[e].as_double_tuple_if_exists(c)
-					? Color(c.at(0), c.at(1), c.at(2))
-					: Color();
+		while (getline(f, line)) {
+			line_i++;
+			istringstream s(line);
+			istream_iterator<string> it(s);
+			auto end = istream_iterator<string>();
+			if (it == end && *it == "newmtl") {
+				break;
+			}
+
+			auto next_string = [&]() {
+				++it;
+				if (it == end) {
+					throw WavefrontParseException(line_i, "expected token");
+				}
+				return *it;
 			};
-			mat.ambient = try_from_conf("Ka");
-			mat.diffuse = try_from_conf("Kd");
-			mat.specular = try_from_conf("Ks");
+			auto next_double = [&]() {
+				return stod(next_string());
+			};
+			auto maybe_next_double = [&]() {
+				++it;
+				if (it == end) {
+					return numeric_limits<double>::signaling_NaN();
+				}
+				return stod(*it);
+			};
+			auto no_next = [&]() {
+				++it;
+				if (it != end) {
+					throw WavefrontParseException(line_i, "too many tokens");
+				}
+			};
+
+			if (*it == "Ka" || *it == "Kd" || *it == "Ks") {
+				char t = (*it)[1];
+				auto r = next_double();
+				auto g = next_double();
+				auto b = next_double();
+				switch (t) {
+				case 'a': mat.ambient = { r, g, b }; break;
+				case 'd': mat.diffuse = { r, g, b }; break;
+				case 's': mat.specular = { r, g, b }; break;
+				default: UNREACHABLE;
+				}
+			} else if (*it == "Ns") {
+				mat.reflection = next_double();
+			} else if (*it == "map_Ka" || *it == "map_Kd" || *it == "map_Ks") {
+				// TODO do we need to support per-light type textures? It seems
+				// excessive...
+				auto path = next_string();
+				if (!mat.texture.has_value()) {
+					img::EasyImage img;
+					ifstream f(path);
+					f >> img;
+					mat.texture.emplace(Texture(std::move(img)));
+				}
+			} else {
+				// TODO we shouldn't ignore other properties.
+				continue;
+			}
+			no_next();
 		}
 	}
 
